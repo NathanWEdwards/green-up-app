a4d827483a8fffa7e6a4cacd8f5b89884628c387
NathanWEdwards <Nathan.W.Edwards@Outlook.com>
2026-02-05 16:33:05 -0500

    Add a dynamic Expo configuration.

---
+++ app.config.ts
@@ -0,0 +1,65 @@
+import { ConfigContext, ExpoConfig } from 'expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+    return {
+        ...config,
+        name: 'green.up',
+        slug: 'green-up',
+        scheme: 'green-up',
+        newArchEnabled: true,
+        version: '1.0.0',
+        orientation: 'portrait',
+        icon: './assets/images/app-icon.png',
+        userInterfaceStyle: 'automatic',
+        splash: {
+            image: './assets/images/splash.png',
+            resizeMode: 'contain',
+            backgroundColor: '#ffffff'
+        },
+        updates: {
+            fallbackToCacheTimeout: 0
+        },
+        assetBundlePatterns: [
+            '**/*'
+        ],
+        ios: {
+            supportsTablet: true,
+            bundleIdentifier: 'org.greenupvermont.app'
+        },
+        android: {
+            adaptiveIcon: {
+                backgroundColor: '#E6F4FE',
+                foregroundImage: './assets/images/android-icon-foreground.png',
+                backgroundImage: './assets/images/android-icon-background.png',
+                monochromeImage: './assets/images/android-icon-monochrome.png'
+            },
+            edgeToEdgeEnabled: true,
+            predictiveBackGestureEnabled: true,
+            package: 'org.greenupvermont.app'
+        },
+        web: {
+            output: 'static',
+            favicon: './assets/images/favicon.png'
+        },
+        plugins: [
+            'expo-router',
+            [
+                'expo-splash-screen',
+                {
+                    image: './assets/images/splash-icon.png',
+                    imageWidth: 200,
+                    resizeMode: 'contain',
+                    backgroundColor: '#ffffff',
+                    dark: {
+                        backgroundColor: '#000000'
+                    }
+                }
+            ],
+            '@react-native-firebase/app'
+        ],
+        experiments: {
+            typedRoutes: true,
+            reactCompiler: true
+        }
+    };
+}
