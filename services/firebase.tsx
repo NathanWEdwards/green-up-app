c7e87ab9a0cc2f1d43a29f83b845a19e74ae693a
NathanWEdwards <Nathan.W.Edwards@Outlook.com>
2026-02-21 17:30:12 -0500

    Add Firebase authentication.

--- services/firebase.tsx
+++ services/firebase.tsx
@@ -1,13 +1,19 @@
 import { getApp } from '@react-native-firebase/app';
+import { connectAuthEmulator, getAuth } from '@react-native-firebase/auth';
 import { connectFirestoreEmulator, getFirestore } from '@react-native-firebase/firestore';
 import { HttpsCallable, getFunctions, httpsCallable } from '@react-native-firebase/functions';
 
 export const app = getApp();
-export const db = getFirestore();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const functions = getFunctions(app, 'us-central1');
 export const callableNearbySites: HttpsCallable = httpsCallable(functions, 'nearbySites');
 
 if (__DEV__ && process.env.USE_EMULATORS) {
+    connectAuthEmulator(
+        auth,
+        `http://${process.env.EMULATOR_HOST || 'localhost'}:${process.env.AUTH_EMULATOR_PORT || '9099'}`
+    );
     connectFirestoreEmulator(
         db,
         process.env.EMULATOR_HOST || 'localhost',
