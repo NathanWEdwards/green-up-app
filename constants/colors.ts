668ad3f99f518a543068656ffd8ce971b20566e5
NathanWEdwards <Nathan.W.Edwards@Outlook.com>
2026-02-26 16:47:32 -0500

    Update all constants.

---
+++ constants/colors.ts
@@ -0,0 +1,24 @@
+const tintColor = "#2f95dc";
+
+export default Object.freeze({
+    transparent: 'transparent',
+    white: "#fff",
+    black: "#000",
+    inputText: "#666",
+    buttonText: "#555",
+    textDark: "#222",
+    backgroundLight: "#EEE",
+    backgroundDark: "#2c2c2c",
+    placeholderText: "#bbb",
+    errorBackground: "red",
+    errorText: "#fff",
+    iconColor: "#007AFF",
+    noticeBackground: tintColor,
+    noticeText: "#fff",
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColor,
+    tabBar: "#fefefe",
+    tintColor,
+    warningBackground: "#EAEB5E",
+    warningText: "#666804"
+});
