0480e5de2f6d97172d52738a8d1ceea686152680
NathanWEdwards <Nathan.W.Edwards@Outlook.com>
2026-03-16 22:56:43 -0400

    store: add SupplyDistribtuionSites tag type.

--- store/slices/apiSlice.ts
+++ store/slices/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react';
 const apiSlice = createApi({
     baseQuery: fakeBaseQuery(),
     endpoints: () => ({}),
-    tagTypes: []
+    tagTypes: ['SupplyDistributionSites']
 });
 
 export default apiSlice;
